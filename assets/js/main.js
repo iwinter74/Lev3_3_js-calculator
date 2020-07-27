5bf5af2c820229d77c02d465bd911101340fbeff
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-07-27 19:17:54 +0200

    v.0.1.1

---
+++ assets/js/main.js
@@ -0,0 +1,140 @@
+let x = ""
+let y
+
+function myFunct9() {
+    console.log("working")
+    y = 9
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct8() {
+    console.log("working")
+    y = 8
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct7() {
+    console.log("working")
+    y = 7
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct6() {
+    console.log("working")
+    y = 6
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct5() {
+    console.log("working")
+    y = 5
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct4() {
+    console.log("working")
+    y = 4
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct3() {
+    console.log("working")
+    y = 3
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct2() {
+    console.log("working")
+    y = 2
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct1() {
+    console.log("working")
+    y = 1
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myFunct0() {
+    console.log("working")
+    y = 0
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myReset() {
+    console.log("reset")
+    document.getElementById("input").innerHTML = ""
+    x = 0
+    y = 0
+    console.log(x)
+    console.log(y)
+}
+function myDot() {
+    console.log("working")
+    y = "."
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myPlus() {
+    console.log("working")
+    y = "+"
+    console.log(y)
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myMinus() {
+    console.log("working")
+    y = "-"
+    console.log(y)
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myMult() {
+    console.log("working")
+    y = "*"
+    console.log(y)
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myDivide() {
+    console.log("working")
+    y = "/"
+    console.log(y)
+    x = x + y
+    console.log(x)
+    console.log(y)
+    document.getElementById("input").innerHTML = x
+}
+function myCalc() {
+    console.log("working")
+    console.log(x)
+    document.getElementById("input").innerHTML = eval(x.toString())
+}
